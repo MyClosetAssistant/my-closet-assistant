343d1ebb99119b0fdff78dbff99aca1448116890
Leo Villalobos <villaleobos@gmail.com>
2023-04-10 21:17:38 -0700

    Create XCode project

---
+++ MyCloset Assistant/Views/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MyCloset Assistant
+//
+//  Created by Leonardo Villalobos on 4/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Overrides
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
