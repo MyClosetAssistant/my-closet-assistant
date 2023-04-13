4710004585ebcdbf41db866fed7e1e3f2a528dab
Leo Villalobos <villaleobos@gmail.com>
2023-04-13 13:46:26 -0700

    Add RegisterView

---
+++ MyCloset Assistant/Views/RegisterView.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterView.swift
+//  MyCloset Assistant
+//
+//  Created by Leonardo Villalobos on 4/13/23.
+//
+
+import UIKit
+
+class RegisterView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
