db7bac6a8403d4e52dd86bccc027bb40a455fd28
Leo Villalobos <villaleobos@gmail.com>
2023-04-13 20:33:06 -0700

    Create Categories and Brands tab views

---
+++ MyCloset Assistant/Views/Tabs/BrandsTabView.swift
@@ -0,0 +1,18 @@
+//
+//  BrandsTabView.swift
+//  MyCloset Assistant
+//
+//  Created by Leonardo Villalobos on 4/13/23.
+//
+
+import UIKit
+
+class BrandsTabView: UIViewController {
+
+    // MARK: Overrides
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ MyCloset Assistant/Views/Tabs/CategoriesTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesTabView.swift
+//  MyCloset Assistant
+//
+//  Created by Leonardo Villalobos on 4/13/23.
+//
+
+import UIKit
+
+class CategoriesTabView: UIViewController {
+
+    // MARK: Overrides
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
