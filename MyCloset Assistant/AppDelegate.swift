6ddec22018f8e4f07c1128bfdf7b43b3378fa573
Leo Villalobos <villaleobos@gmail.com>
2023-04-13 20:33:46 -0700

    Initialize ParseSwift in AppDelegate

--- MyCloset Assistant/AppDelegate.swift
+++ MyCloset Assistant/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         // Override point for customization after application launch.
+        initializeParse()
         return true
     }
 
