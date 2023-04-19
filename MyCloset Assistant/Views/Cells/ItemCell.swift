8eb4a482a8a0ad617cf43475ba46507a36cb9f4a
Leo Villalobos <villaleobos@gmail.com>
2023-04-19 14:05:04 -0700

    Create ItemCell for each TableViewCell in ResultsView

---
+++ MyCloset Assistant/Views/Cells/ItemCell.swift
@@ -0,0 +1,26 @@
+//
+//  ItemCell.swift
+//  MyCloset Assistant
+//
+//  Created by Leonardo Villalobos on 4/18/23.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+   
+    @IBOutlet weak var itemNameLabel: UILabel!
+    @IBOutlet weak var itemCategoriesLabel: UILabel!
+    static let reusableId = "ItemCell"
+    
+    // MARK: Overrides
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
