805c1bd78fd4c171974c89c909d27f061417fc5e
SwiftReformatter <villaleobos@gmail.com>
2023-04-27 01:57:25 +0000

    Reformat code via GitHub Action swift-fmt

--- MyCloset Assistant/Views/LoginView.swift
+++ MyCloset Assistant/Views/LoginView.swift
@@ -11,67 +11,73 @@ class LoginView: UIViewController {
 
   @IBOutlet weak var usernameField: UITextField!
   @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var usernameErrorLabel: UILabel!
-    @IBOutlet weak var passwordErrorLabel: UILabel!
-    @IBOutlet weak var loginErrorLabel: UILabel!
-    
-    let queue = DispatchQueue.global(qos: .userInitiated)
-    
-    // MARK: Overrides
+  @IBOutlet weak var usernameErrorLabel: UILabel!
+  @IBOutlet weak var passwordErrorLabel: UILabel!
+  @IBOutlet weak var loginErrorLabel: UILabel!
+
+  let queue = DispatchQueue.global(qos: .userInitiated)
+
+  // MARK: Overrides
 
   override func viewDidLoad() {
     super.viewDidLoad()
-      usernameErrorLabel.isHidden = true
-      passwordErrorLabel.isHidden = true
-      loginErrorLabel.isHidden = true
+    usernameErrorLabel.isHidden = true
+    passwordErrorLabel.isHidden = true
+    loginErrorLabel.isHidden = true
   }
 
   @IBAction func onLoginTapped(_ sender: Any) {
-      let username = usernameField!.text!
-      let password = passwordField!.text!
-      
-      if username.isEmpty {
-          usernameErrorLabel.text = "'Username' field must be filled out"
-          showErrorLabel(for: &usernameErrorLabel)
+    let username = usernameField!.text!
+    let password = passwordField!.text!
+
+    if username.isEmpty {
+      usernameErrorLabel.text = "'Username' field must be filled out"
+      showErrorLabel(for: &usernameErrorLabel)
+    }
+    if password.isEmpty {
+      passwordErrorLabel.text = "'Password' field must be filled out"
+      showErrorLabel(for: &passwordErrorLabel)
+    }
+
+    User.login(username: username, password: password) { [unowned self] result in
+      if !username.isEmpty {
+        self.hideErrorLabel(for: &usernameErrorLabel)
       }
-      if password.isEmpty {
-          passwordErrorLabel.text = "'Password' field must be filled out"
-          showErrorLabel(for: &passwordErrorLabel)
+      if !password.isEmpty {
+        self.hideErrorLabel(for: &passwordErrorLabel)
       }
 
-      User.login(username: username, password: password) { [unowned self] result in
-        if !username.isEmpty {
-          self.hideErrorLabel(for: &usernameErrorLabel)
-        }
-        if !password.isEmpty {
-          self.hideErrorLabel(for: &passwordErrorLabel)
-        }
-          
-        switch result {
-        case .success(let user):
-          print("Logged in as user: \(user)")
-          self.hideErrorLabel(for: &loginErrorLabel)
-            queue.asyncAfter(deadline: .now() + 1, execute: {
-                NotificationCenter.default.post(name: Notification.Name("login"), object: nil)
-            })
-        case .failure(let error):
-          self.loginErrorLabel.text = error.message.capitalized
-          self.showErrorLabel(for: &self.loginErrorLabel)
-        }
+      switch result {
+      case .success(let user):
+        print("Logged in as user: \(user)")
+        self.hideErrorLabel(for: &loginErrorLabel)
+        queue.asyncAfter(
+          deadline: .now() + 1,
+          execute: {
+            NotificationCenter.default.post(name: Notification.Name("login"), object: nil)
+          })
+      case .failure(let error):
+        self.loginErrorLabel.text = error.message.capitalized
+        self.showErrorLabel(for: &self.loginErrorLabel)
       }
+    }
   }
-    
-    // MARK: Private Helpers
 
-     private func showErrorLabel(for label: inout UILabel) {
-        UILabel.transition(with: label, duration: 0.33, options: [.transitionCrossDissolve], animations: { [label] in
-            label.isHidden = false
-        })
-    }
-    
-    private func hideErrorLabel(for label: inout UILabel) {
-        UILabel.animate(withDuration: 0.33, animations: { [label] in
-            label.isHidden = true
-        })
-    }
+  // MARK: Private Helpers
+
+  private func showErrorLabel(for label: inout UILabel) {
+    UILabel.transition(
+      with: label, duration: 0.33, options: [.transitionCrossDissolve],
+      animations: { [label] in
+        label.isHidden = false
+      })
+  }
+
+  private func hideErrorLabel(for label: inout UILabel) {
+    UILabel.animate(
+      withDuration: 0.33,
+      animations: { [label] in
+        label.isHidden = true
+      })
+  }
 }
